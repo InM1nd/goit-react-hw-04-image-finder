4bc12026b618e91ce27b5ac130d094171777ec48
InM1nd <zabolotny1111@gmail.com>
2022-10-09 17:50:39 +0200

    done

---
+++ src/components/App.jsx
@@ -0,0 +1,99 @@
+import {  useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Searchbar from '../components/Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+
+export function App ()  {
+  const [request, setRequest] = useState('');
+  const [pictures, setPicture] = useState([]);
+  const [totalPictures, setTotalPictures] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImg, setModalImg] = useState(null);
+  const [modalAlt, setModalAlt] = useState(null);
+
+
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
+    };
+
+  const handleModalImg = ({ largeImageURL, tags }) => {
+    setModalImg(largeImageURL);
+    setModalAlt(tags);
+  };
+
+  const handleRequestValue = requestValue => {
+    setRequest(requestValue.trim())
+    setPage(1);
+    setPicture([]);
+  };
+
+useEffect(() => {
+  if (request === '') {
+    return;
+  }
+  const getApi = async () => {
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?q=${request}&page=${page}&key=26520489-9dedc914612f42cfe7de51211&image_type=photo&orientation=horizontal&per_page=12`
+      );
+      return response.data;
+    } catch (error) {
+      setError(error);
+      setStatus('rejected');
+    }
+  };
+
+  setStatus('pending');
+  getApi().then(response => {
+    if (response && !response.hits.length) {
+      setStatus('idle');
+      return toast.error('There is no images found. Please try again!');
+    }
+      response.hits.forEach(picture => {
+        setPicture(prevState => [...prevState, picture]);
+        setTotalPictures(response.total);
+        setStatus('resolve');
+      });
+  });
+},[page,request]);
+
+  const loadMore = () => {
+    setPage(prevState => prevState.page + 1); 
+  }
+
+    return (
+      <>
+        <Searchbar onSubmit={handleRequestValue} />
+        {pictures.length !== 0 && (
+          <ImageGallery
+            pictures={pictures}
+            onClick={toggleModal}
+            onHandleModalImg={handleModalImg}
+          />
+        )}
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <img src={modalImg} alt={modalAlt} />
+          </Modal>
+        )}
+        {status === 'pending' && <Loader />}
+        {status !== 'pending' &&
+          pictures.length !== 0 &&
+          totalPictures !== pictures.length && (
+            <Button loadMore={loadMore} />
+          )}
+        <ToastContainer autoClose={3000} />
+        {status === 'rejected' &&
+          toast.error(`Whoops, something went wrong: ${error.message}`)}
+      </>
+    );
+  }
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import React from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+
+function Modal ({onClose, children}) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  },[onClose]);
+
+  const handleOverlay = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  }
+
+
+    return createPortal(
+      <div className={s.Overlay} onClick={handleOverlay}>
+        <div className={s.Modal}>{children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+
+
+Modal.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
